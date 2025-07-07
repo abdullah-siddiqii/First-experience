6b7fd75d5a8d26f6163dc2b912aec007fe155198
siddiqui258 <siddiquimabdullah2@gmail.com>
2025-07-07 16:46:50 +0500

    qwertyui

--- app/page.tsx
+++ app/page.tsx
@@ -18,28 +18,28 @@ export default function Page() {
           image="https://demo-source.imgix.net/snowboard.jpg?w=7094&h=5157&rect=1098%2C303%2C7094%2C5157"
           title="Premium Cafe"
           description="For coffee lovers"
-          price={10}
+          price={500}
           onTotalChange={handleTotalChange}
         />
         <Home
           image="https://demo-source.imgix.net/sneakers.jpg"
           title="Premium Shoes"
           description="For shoe lovers"
-          price={25}
+          price={2500}
           onTotalChange={handleTotalChange}
         />
         <Home
           image="https://demo-source.imgix.net/plant.jpg?w=4160&h=4520&rect=0%2C1051%2C4160%2C4520"
           title="Premium Plant"
           description="For plant lovers"
-          price={15}
+          price={1500}
           onTotalChange={handleTotalChange}
         />
         <Home
           image="https://demo-source.imgix.net/head_shot.jpg"
           title="Premium Clothes"
           description="For clothes lovers"
-          price={20}
+          price={2000}
           onTotalChange={handleTotalChange}
         />
 
@@ -47,28 +47,28 @@ export default function Page() {
           image="https://demo-source.imgix.net/group_photo.jpg"
           title="Premium Group"
           description="For Group Lovers"
-          price={10}
+          price={10000}
           onTotalChange={handleTotalChange}
         />
                <Home
           image="https://demo-source.imgix.net/puppy.jpg"
           title="Premium Pet"
           description="For Dog Lovers"
-          price={10}
+          price={4000}
           onTotalChange={handleTotalChange}
         />
                    <Home
           image="https://demo-source.imgix.net/canyon.jpg"
           title="Premium Hills"
           description="For Hills Lovers"
-          price={17}
+          price={17000}
           onTotalChange={handleTotalChange}
         />
                       <Home
           image="https://demo-source.imgix.net/scooter.jpg"
           title="Premium Motors"
           description="For Scooter Lovers"
-          price={10}
+          price={100000}
           onTotalChange={handleTotalChange}
         />
       </div>
